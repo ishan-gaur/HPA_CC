051720ed223ed92679c77070973a45674bee7d6e
Ishan Gaur <ishangaur4@gmail.com>
2024-01-26 18:16:32 +0100

    heuristic pseudotime label and plotting well-level fucci time and angular distributions

--- scripts/pseudotime_labels.py
+++ scripts/pseudotime_labels.py
@@ -3,7 +3,7 @@ Takes as a parameter the name of the post-processed data version to use
 Iterate well by well and get the total number of cells using the sc tensor first dimension
 Need to load all the images in order of the index and apply the nuclear mask to get the total intensity
 """
-
+import numpy as np
 import math
 import torch
 import pickle as pkl
@@ -11,6 +11,8 @@ from tqdm import tqdm
 from HPA_CC.data.dataset import DatasetFS
 from HPA_CC.utils.pseudotime import intensities_to_pseudotime
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 from config import FUCCI_DS_PATH, FUCCI_NAME, OUTPUT_DIR
 from multiprocessing import Pool
 
@@ -26,8 +28,8 @@ plt.close()
 
 # plot a grid of the polar distrbution of each well
 n_wells = len(fucci_ds.well_list)
-if (OUTPUT_DIR / "well_intensity_cache.pkl").exists() and False:
-    cells_per_well, well_intensities, well_pseudotimes = pkl.load(open(OUTPUT_DIR / "well_intensity_cache.pkl", "rb"))
+if (OUTPUT_DIR / "well_intensity_cache.pkl").exists():
+    cells_per_well, well_intensities, well_pseudotimes, well_angles = pkl.load(open(OUTPUT_DIR / "well_intensity_cache.pkl", "rb"))
 else:
     def process_well(well):
         sc_images = torch.load(well / f"images_{FUCCI_NAME}.pt") # Cells x Channels x H x W
@@ -39,17 +41,16 @@ else:
         log_mean_GMNN = torch.log(mean_intensities[:, 0] + min_nonzero_GMNN)
         log_mean_CDT1 = torch.log(mean_intensities[:, 1] + min_nonzero_CDT1)
         log_mean_fucci_intensities = torch.stack((log_mean_GMNN, log_mean_CDT1), dim=1)
-        pseudotime, center = intensities_to_pseudotime(log_mean_fucci_intensities.numpy())
-        return len(sc_images), log_mean_fucci_intensities, pseudotime
+        fucci_time, raw_time, center = intensities_to_pseudotime(log_mean_fucci_intensities.numpy())
+        return len(sc_images), log_mean_fucci_intensities, fucci_time, raw_time
 
-    with Pool(1) as pool:
+    with Pool(16) as pool:
         results = list(tqdm(pool.imap(process_well, fucci_ds.well_list), total=n_wells, desc="Plotting Pseudotime distributions"))
 
-    cells_per_well, well_intensities, well_pseudotimes = zip(*results)
+    cells_per_well, well_intensities, well_pseudotimes, well_angles = zip(*results)
 
-    pkl.dump((cells_per_well, well_intensities, well_pseudotimes), open(OUTPUT_DIR / "well_intensity_cache.pkl", "wb"))
+    pkl.dump((cells_per_well, well_intensities, well_pseudotimes, well_angles), open(OUTPUT_DIR / "well_intensity_cache.pkl", "wb"))
 
-print(n_wells, "wells")
 n_col = 12
 n_row = math.ceil(n_wells / n_col)
 plt.clf()
@@ -58,7 +59,35 @@ for i, well, cells, intensities, pseudotime in zip(range(n_wells), fucci_ds.well
     ax = axes[i // n_col, i % n_col]
     ax.set_title(f"{well.name} n={cells}")
     ax.scatter(intensities[:, 0], intensities[:, 1], c=pseudotime, cmap="RdYlGn")
-plt.savefig(OUTPUT_DIR / "pseudotime_distributions.png")
+plt.savefig(OUTPUT_DIR / "fucci_plot_pseudotimes.png")
+plt.close()
+
+
+bin_res = 0.05
+pseudotime_bins = np.arange(0, 1 + bin_res, bin_res)
+n_col = 12
+n_row = math.ceil(n_wells / n_col)
+plt.clf()
+fig, axes = plt.subplots(n_row, n_col, figsize=(n_col * 4, n_row * 4), sharex=True, sharey=True)
+for i, well, cells, intensities, pseudotime in zip(range(n_wells), fucci_ds.well_list, cells_per_well, well_intensities, well_pseudotimes):
+    binned_pseudotime = (np.digitize(pseudotime, pseudotime_bins) - 0.5) * bin_res
+    well_df = pd.DataFrame({"GMNN": intensities[:, 0], "CDT1": intensities[:, 1], "pseudotime": binned_pseudotime})
+    ax = axes[i // n_col, i % n_col]
+    ax.set_title(f"{well.name} n={cells}")
+    sns.lineplot(x="pseudotime", y="GMNN", data=well_df, ax=ax, color="green")
+    sns.lineplot(x="pseudotime", y="CDT1", data=well_df, ax=ax, color="red")
+plt.savefig(OUTPUT_DIR / "average_intensity_pseudotime.png")
+plt.close()
+
+n_col = 12
+n_row = math.ceil(n_wells / n_col)
+plt.clf()
+fig, axes = plt.subplots(n_row, n_col, figsize=(n_col * 4, n_row * 4), sharex=True, sharey=False)
+for i, well, cells, intensities, angle in zip(range(n_wells), fucci_ds.well_list, cells_per_well, well_intensities, well_angles):
+    ax = axes[i // n_col, i % n_col]
+    ax.set_title(f"{well.name} n={cells}")
+    ax.hist(angle, bins=50)
+plt.savefig(OUTPUT_DIR / "angular_distribution.png")
 plt.close()
 
 plt.hist(cells_per_well)
--- src/HPA_CC/utils/pseudotime.py
+++ src/HPA_CC/utils/pseudotime.py
@@ -41,8 +41,8 @@ def intensities_to_pseudotime(log_intensities, center=None):
     r = np.sqrt(np.sum(centered_intensities ** 2, axis=1))
     theta = np.arctan2(centered_intensities[:, 1], centered_intensities[:, 0])
     polar = np.stack([r, theta], axis=-1)
-    fucci_time = calculate_pseudotime(polar.T, centered_intensities)
-    return fucci_time, center
+    fucci_time, raw_time = calculate_pseudotime(polar.T, centered_intensities)
+    return fucci_time, raw_time, center
 
 def min_angle_diff(a, b):
     return min((a - b) % (2 * np.pi), (b - a) % (2 * np.pi))
@@ -50,74 +50,46 @@ def min_angle_diff(a, b):
 def calculate_pseudotime(pol_data, centered_data, save_dir=""):
     r, theta = pol_data
     x, y = centered_data.T
-    # note that pol_data is coord x cell, centered_data is cell x coord
-    # find the index of the point that is closest 3/2 pi
-    lower_ind = np.argmin(np.array([min_angle_diff(t, 3 / 2 * np.pi) for t in theta]))
-    # find the index of the cell most depleted in GMNN, this is likely in G1
-    leftmost_ind = np.argmin(x)
-
-    # reindex theta so that theta for lower_ind is maximal (2pi), and theta is in (0, 2pi]
-    theta -= theta[lower_ind]
-    theta[theta < 0] += 2 * np.pi
-    theta[lower_ind] = 2 * np.pi
+    start_theta = np.arctan2(np.min(y), -1)
 
     # sort by theta
     pol_sort_inds = np.argsort(theta)
-    leftmost_ind = np.where(pol_sort_inds == leftmost_ind)[0][0]
-    lower_ind = np.where(pol_sort_inds == lower_ind)[0][0]
-    print(leftmost_ind, lower_ind, len(pol_sort_inds))
     pol_sort_r = pol_data[0][pol_sort_inds]
     pol_sort_theta = pol_data[1][pol_sort_inds]
     pol_sort_x = x[pol_sort_inds]
     pol_sort_y = y[pol_sort_inds]
 
-    # Rezero to minimum--reasoning, cells disappear during mitosis, so we should have the fewest detected cells there
-    cc_props = FucciCellCycle()
-    n_bins = int(cc_props.TOT_LEN / cc_props.M_LEN)
-    n_bins_sector = int(n_bins / 8) # looking for something in the lower bottom left octant
-    # lower_octant_theta = np.concatenate((pol_sort_theta[leftmost_ind:], pol_sort_theta[:lower_ind + 1]))
-    # lower_octant_x, lower_octant_y = np.concatenate((pol_sort_x[leftmost_ind:], pol_sort_x[:lower_ind + 1])), np.concatenate((pol_sort_y[leftmost_ind:], pol_sort_y[:lower_ind + 1]))
-    lower_octant_theta = pol_sort_theta[leftmost_ind:]
-    lower_octant_x, lower_octant_y = pol_sort_x[leftmost_ind:], pol_sort_y[leftmost_ind:]
-    plt.clf()
-    plt.scatter(x, y, alpha=0.5)
-    plt.scatter(lower_octant_x, lower_octant_y, c=(lower_octant_theta - np.min(lower_octant_theta)) / (2 * np.pi), cmap="RdYlGn")
-    plt.savefig("ping.png")
-
-    plt.clf()
-    bin_vals, bin_edges, patches = plt.hist(lower_octant_theta, min(n_bins_sector, len(lower_octant_theta)))
-    plt.close()
-    start_phi = bin_edges[np.argmin(bin_vals)]
-    # start_phi = bin_edges[np.max(np.where(bin_vals == np.min(bin_vals))[0])]
-
     # Move those points to the other side
-    more_than_start = np.greater(pol_sort_theta, start_phi)
-    less_than_start = np.less_equal(pol_sort_theta, start_phi)
+    more_than_start = np.greater(pol_sort_theta, start_theta)
+    less_than_start = np.less_equal(pol_sort_theta, start_theta)
     pol_sort_rho_reorder = np.concatenate(
         (pol_sort_r[more_than_start], pol_sort_r[less_than_start])
     )
     pol_sort_inds_reorder = np.concatenate(
         (pol_sort_inds[more_than_start], pol_sort_inds[less_than_start])
     )
-    pol_sort_phi_reorder = np.concatenate(
+    pol_sort_theta_reorder = np.concatenate(
         (pol_sort_theta[more_than_start], pol_sort_theta[less_than_start] + np.pi * 2)
     )
-    pol_sort_centered_data0 = np.concatenate(
+    pol_sort_x_reorder = np.concatenate(
         (pol_sort_x[more_than_start], pol_sort_x[less_than_start])
     )
-    pol_sort_centered_data1 = np.concatenate(
+    pol_sort_y_reorder = np.concatenate(
         (pol_sort_y[more_than_start], pol_sort_y[less_than_start])
     )
-    pol_sort_shift = pol_sort_phi_reorder + np.abs(np.min(pol_sort_phi_reorder))
+    pol_sort_shift = pol_sort_theta_reorder + np.abs(np.min(pol_sort_theta_reorder))
+
 
     # Shift and re-scale "time"
     # reverse "time" since the cycle goes counter-clockwise wrt the fucci plot
     pol_sort_norm = pol_sort_shift / np.max(pol_sort_shift)
     pol_sort_norm_rev = 1 - pol_sort_norm
+    raw_time = pol_sort_norm_rev.copy()
     pol_sort_norm_rev = stretch_time(pol_sort_norm_rev)
+    pol_sort_norm_rev[0] = 1.0
     pol_unsort = np.argsort(pol_sort_inds_reorder)
     fucci_time = pol_sort_norm_rev[pol_unsort]
-    return fucci_time
+    return fucci_time, raw_time
 
 def stretch_time(time_data,nbins=1000):
     # This function creates a uniform across pseudotime
