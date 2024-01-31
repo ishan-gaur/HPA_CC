c180444c72f093b25148352a3de5bdbe4cb6df94
Ishan Gaur <ishangaur4@gmail.com>
2024-01-31 20:28:40 +0100

    migrate to a ipython-based approach

--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,6 @@ FUCCI_DS_PATH = Path('/data/ishang/FUCCI-dataset-well')
 FUCCI_NAME = "unnormalize_1250_sharp_512_crop_og_res"
 HPA_DS_PATH = Path('/data/ishang/all_HPA-CC-dataset')
 
-OUTPUT_DIR = Path.cwd() / "scripts" / "output"
+OUTPUT_DIR = Path.cwd() / "output"
 if not OUTPUT_DIR.exists():
     OUTPUT_DIR.mkdir()
--- scripts/dataset_summary.py
+++ scripts/dataset_summary.py
@@ -20,153 +20,194 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from config import FUCCI_DS_PATH, HPA_DS_PATH, OUTPUT_DIR
+from config import OUTPUT_DIR, FUCCI_DS_PATH, HPA_DS_PATH
 from HPA_CC.data.dataset import DatasetFS
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 
-fucci_ds = DatasetFS(FUCCI_DS_PATH)
-hpa_ds = DatasetFS(HPA_DS_PATH)
 
-well_type_dict = {}
+def load_dataset_fs(dataset_name):
+    if dataset_name == "fucci":
+        return DatasetFS(FUCCI_DS_PATH)
+    elif dataset_name == "hpa":
+        return DatasetFS(HPA_DS_PATH)
+    else:
+        raise ValueError(f"Invalid dataset name {dataset_name}")
 
-for f in fucci_ds.well_list:
-    well_type = f.name.split('--')[0]
-    if not well_type in well_type_dict:
-        well_type_dict[well_type] = []
-    well_type_dict[well_type].append(len(list(filter(lambda d: d.is_dir(), f.iterdir()))))
-
-well_type_dict["hpa"] = []
-for f in hpa_ds.well_list:
-    well_type_dict["hpa"].append(len(list(filter(lambda d: d.is_dir(), f.iterdir()))))
-    num_images = len(list(filter(lambda d: d.is_dir(), f.iterdir())))
-
-print("Number of images per well, grouped by microscope/run")
-for scope, well_img_cts in well_type_dict.items():
-    print(scope)
-    print(f"\tNumber of wells: {len(well_img_cts)}")
-    print(f"\tMean number of images per well: {np.mean(well_img_cts):.1f}")
-    print(f"\t{well_img_cts}")
-
-bits = 8
-percentiles = np.arange(0, 100, 100 / (2 ** bits))
-
-intensity_files = ["nuc_percentiles.npy", "mt_percentiles.npy", "microscopes_wells.pkl"]
-for dataset_name, dataset in zip(["fucci", "hpa"], [fucci_ds, hpa_ds]):
+# goes through the dataset and computes the intensity percentiles for each image's channels
+# these are then saved to a file for later use, 
+def get_dataset(dataset_name):
+    dataset = load_dataset_fs(dataset_name)
+    intensity_files = ["nuc_percentiles.npy", "mt_percentiles.npy", "microscopes_wells.pkl"]
     cached_intensities_exist = all([Path.exists(OUTPUT_DIR / f"{dataset_name}_{f}") for f in intensity_files])
-    if not cached_intensities_exist:
-        microscopes, wells = [], []
-        nuc_percentiles, mt_percentiles = [[] for _ in range(2)]
+
+    if cached_intensities_exist:
+        microscopes, wells = pkl.load(open(OUTPUT_DIR / f"{dataset_name}_microscopes_wells.pkl", "rb"))
+        nuc_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_nuc_percentiles.npy")
+        mt_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_mt_percentiles.npy")
         if dataset_name == "fucci":
-            cdt1_percentiles, gmnn_percentiles = [[] for _ in range(2)]
-        if dataset_name == "hpa":
-            er_percentiles = []
+            cdt1_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_cdt1_percentiles.npy")
+            gmnn_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_gmnn_percentiles.npy")
+            return microscopes, wells, nuc_percentiles, mt_percentiles, cdt1_percentiles, gmnn_percentiles
+        elif dataset_name == "hpa":
+            er_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_er_percentiles.npy")
+            return microscopes, wells, nuc_percentiles, mt_percentiles, er_percentiles
+        else:
+            raise ValueError(f"Invalid dataset name {dataset_name}")
+        
+    manager = Manager()
+    microscopes = manager.list()
+    wells = manager.list()
 
-        def get_percentiles_nonzero(img_path):
-            img = np.array(Image.open(img_path))
-            img = img.flatten()
-            img = img[img > 0]
-            return np.percentile(img, percentiles)
+    # define lists for channels needed per dataset
+    nuc_percentiles = manager.list()
+    mt_percentiles = manager.list()
+    if dataset_name == "fucci":
+        cdt1_percentiles = manager.list()
+        gmnn_percentiles = manager.list()
+        percentile_lists = [nuc_percentiles, mt_percentiles, cdt1_percentiles, gmnn_percentiles]
+    if dataset_name == "hpa":
+        er_percentiles = manager.list()
+        percentile_lists = [nuc_percentiles, mt_percentiles, er_percentiles]
 
-        def process_image(img_dir):
-            well_dir = img_dir.parent
+    # parallelize over images directly and add the percentiles to the lists above
+    with Pool(16) as pool:
+        tasks = []
+        for img_dir in dataset.image_list:
+            tasks.append(pool.apply_async(process_image, (img_dir, dataset_name, microscopes, wells, percentile_lists)))
+        for task in tqdm(tasks, desc=f"Processing {dataset_name} images"):
+            task.get()
+
+    # save the outputs to files, different files depending on the dataset
+    microscopes = list(microscopes)
+    wells = list(wells)
+    nuc_percentiles = list(nuc_percentiles)
+    mt_percentiles = list(mt_percentiles)
+    if dataset_name == "fucci":
+        cdt1_percentiles = list(cdt1_percentiles)
+        gmnn_percentiles = list(gmnn_percentiles)
+    if dataset_name == "hpa":
+        er_percentiles = list(er_percentiles)
+
+    pkl.dump((microscopes, wells), open(OUTPUT_DIR / f"{dataset_name}_microscopes_wells.pkl", "wb"))
+
+    nuc_percentiles = np.array(nuc_percentiles)
+    mt_percentiles = np.array(mt_percentiles)
+    np.save(OUTPUT_DIR / f"{dataset_name}_nuc_percentiles.npy", nuc_percentiles)
+    np.save(OUTPUT_DIR / f"{dataset_name}_mt_percentiles.npy", mt_percentiles)
+
+    if dataset_name == "fucci":
+        cdt1_percentiles = np.array(cdt1_percentiles)
+        gmnn_percentiles = np.array(gmnn_percentiles)
+        np.save(OUTPUT_DIR / f"{dataset_name}_cdt1_percentiles.npy", cdt1_percentiles)
+        np.save(OUTPUT_DIR / f"{dataset_name}_gmnn_percentiles.npy", gmnn_percentiles)
+    if dataset_name == "hpa":
+        er_percentiles = np.array(er_percentiles)
+        np.save(OUTPUT_DIR / f"{dataset_name}_er_percentiles.npy", er_percentiles)
+
+    if dataset_name == "fucci":
+        return microscopes, wells, nuc_percentiles, mt_percentiles, cdt1_percentiles, gmnn_percentiles
+    elif dataset_name == "hpa":
+        return microscopes, wells, nuc_percentiles, mt_percentiles, er_percentiles
+
+# below are the helper functions that process each image and populate the lists above
+# note that the nuclear and microtubule channels have difference extraction code due to 
+# differences in the naming conventions between the two datasets
+
+def process_image(img_dir, dataset_name, microscopes, wells, percentile_array_lists):
+    well_dir = img_dir.parent
+    if dataset_name == "fucci":
+        microscopes.append(well_dir.name.split('--')[0])
+    else:
+        microscopes.append("hpa")
+    wells.append(well_dir.name)
+
+    if dataset_name == "fucci":
+        nuc = get_percentiles_nonzero(img_dir / "nuclei.png")
+        mt = get_percentiles_nonzero(img_dir / "microtubule.png")
+        cdt1 = get_percentiles_nonzero(img_dir / "CDT1.png")
+        gmnn = get_percentiles_nonzero(img_dir / "Geminin.png")
+        image_percentiles = [nuc, mt, cdt1, gmnn]
+    if dataset_name == "hpa":
+        nuc = get_percentiles_nonzero(img_dir / f"{img_dir.name}_blue.png")
+        mt = get_percentiles_nonzero(img_dir / f"{img_dir.name}_red.png")
+        er = get_percentiles_nonzero(img_dir / f"{img_dir.name}_yellow.png")
+        image_percentiles = [nuc, mt, er]
+
+    for percentile_array, channel_percentiles in zip(percentile_array_lists, image_percentiles):
+        percentile_array.append(channel_percentiles)
+
+def get_percentiles_nonzero(img_path):
+    bits = 8
+    percentiles = np.arange(0, 100, 100 / (2 ** bits))
+    img = np.array(Image.open(img_path))
+    img = img.flatten()
+    img = img[img > 0]
+    return np.percentile(img, percentiles)
+
+# below are the functions that load the data from the files created above
+
+def load_fucci_wells():
+    return load_dataset_wells("fucci")
+
+def load_hpa_wells():
+    return load_dataset_wells("hpa")
+
+def load_dataset_wells(dataset_name):
+    if not Path.exists(OUTPUT_DIR / f"{dataset_name}_microscopes_wells.pkl"):
+        dataset = load_dataset_fs(dataset_name)
+        microscopes, wells = [], []
+        for well_dir in dataset.well_list:
             if dataset_name == "fucci":
-                microscopes.append(well_dir.name.split('--')[0])
-            else:
+                    microscopes.append(well_dir.name.split('--')[0])
+            elif dataset_name == "hpa":
                 microscopes.append("hpa")
             wells.append(well_dir.name)
-
-            if dataset_name == "fucci":
-                nuc = get_percentiles_nonzero(img_dir / "nuclei.png")
-                mt = get_percentiles_nonzero(img_dir / "microtubule.png")
-                cdt1 = get_percentiles_nonzero(img_dir / "CDT1.png")
-                gmnn = get_percentiles_nonzero(img_dir / "Geminin.png")
-            if dataset_name == "hpa":
-                nuc = get_percentiles_nonzero(img_dir / f"{img_dir.name}_blue.png")
-                mt = get_percentiles_nonzero(img_dir / f"{img_dir.name}_red.png")
-                er = get_percentiles_nonzero(img_dir / f"{img_dir.name}_yellow.png")
-
-            nuc_percentiles.append(np.percentile(nuc, percentiles))
-            mt_percentiles.append(np.percentile(mt, percentiles))
-            if dataset_name == "fucci":
-                cdt1_percentiles.append(np.percentile(cdt1, percentiles))
-                gmnn_percentiles.append(np.percentile(gmnn, percentiles))
-            if dataset_name == "hpa":
-                er_percentiles.append(np.percentile(er, percentiles))
-
-        with Pool(16) as pool:
-            pool.map(process_image, dataset.image_list)
-
         pkl.dump((microscopes, wells), open(OUTPUT_DIR / f"{dataset_name}_microscopes_wells.pkl", "wb"))
+    microscopes, wells = pkl.load(open(OUTPUT_DIR / f"{dataset_name}_microscopes_wells.pkl", "rb"))
+    return microscopes, wells
 
-        nuc_percentiles = np.array(nuc_percentiles)
-        mt_percentiles = np.array(mt_percentiles)
-        np.save(OUTPUT_DIR / f"{dataset_name}_nuc_percentiles.npy", nuc_percentiles)
-        np.save(OUTPUT_DIR / f"{dataset_name}_mt_percentiles.npy", mt_percentiles)
+def load_fucci_percentiles():
+    return load_dataset_percentiles("fucci")
 
-        if dataset_name == "fucci":
-            cdt1_percentiles = np.array(cdt1_percentiles)
-            gmnn_percentiles = np.array(gmnn_percentiles)
-            np.save(OUTPUT_DIR / f"{dataset_name}_cdt1_percentiles.npy", cdt1_percentiles)
-            np.save(OUTPUT_DIR / f"{dataset_name}_gmnn_percentiles.npy", gmnn_percentiles)
-        if dataset_name == "hpa":
-            er_percentiles = np.array(er_percentiles)
-            np.save(OUTPUT_DIR / f"{dataset_name}_er_percentiles.npy", er_percentiles)
+def load_hpa_percentiles():
+    return load_dataset_percentiles("hpa")
 
-fucci_microscopes, fucci_wells = pkl.load(open(OUTPUT_DIR / f"fucci_microscopes_wells.pkl", "rb"))
-fucci_nuc_percentiles = np.load(OUTPUT_DIR / f"fucci_nuc_percentiles.npy")
-fucci_mt_percentiles = np.load(OUTPUT_DIR / f"fucci_mt_percentiles.npy")
-fucci_cdt1_percentiles = np.load(OUTPUT_DIR / f"fucci_cdt1_percentiles.npy")
-fucci_gmnn_percentiles = np.load(OUTPUT_DIR / f"fucci_gmnn_percentiles.npy")
+def load_dataset_percentiles(dataset_name):
+    nuc_exists = Path.exists(OUTPUT_DIR / f"{dataset_name}_nuc_percentiles.npy")
+    mt_exists = Path.exists(OUTPUT_DIR / f"{dataset_name}_mt_percentiles.npy")
+    if dataset_name == "fucci":
+        cdt1_exists = Path.exists(OUTPUT_DIR / f"{dataset_name}_cdt1_percentiles.npy")
+        gmnn_exists = Path.exists(OUTPUT_DIR / f"{dataset_name}_gmnn_percentiles.npy")
+        perc_files_exist = nuc_exists and mt_exists and cdt1_exists and gmnn_exists
+    elif dataset_name == "hpa":
+        er_exists = Path.exists(OUTPUT_DIR / f"{dataset_name}_er_percentiles.npy")
+        perc_files_exist = nuc_exists and mt_exists and er_exists
 
-hpa_microscopes, hpa_wells = pkl.load(open(OUTPUT_DIR / f"hpa_microscopes_wells.pkl", "rb"))
-hpa_nuc_percentiles = np.load(OUTPUT_DIR / f"hpa_nuc_percentiles.npy")
-hpa_mt_percentiles = np.load(OUTPUT_DIR / f"hpa_mt_percentiles.npy")
-hpa_er_percentiles = np.load(OUTPUT_DIR / f"hpa_er_percentiles.npy")
+    if not perc_files_exist:
+        get_dataset(dataset_name)
 
-microscopes = list(fucci_microscopes) + list(hpa_microscopes)
-wells = list(fucci_wells) + list(hpa_wells)
-nuc_percentiles = np.concatenate((fucci_nuc_percentiles, hpa_nuc_percentiles), axis=0)
-mt_percentiles = np.concatenate((fucci_mt_percentiles, hpa_mt_percentiles), axis=0)
+    nuc_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_nuc_percentiles.npy")
+    mt_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_mt_percentiles.npy")
 
+    if dataset_name == "fucci":
+        cdt1_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_cdt1_percentiles.npy")
+        gmnn_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_gmnn_percentiles.npy")
+        return nuc_percentiles, mt_percentiles, cdt1_percentiles, gmnn_percentiles
+    elif dataset_name == "hpa":
+        er_percentiles = np.load(OUTPUT_DIR / f"{dataset_name}_er_percentiles.npy")
+        return nuc_percentiles, mt_percentiles, er_percentiles
 
-pca = PCA(n_components=2)
-scaler = StandardScaler()
-
-ref_intensities = np.concatenate((nuc_percentiles, mt_percentiles), axis=1)
-ref_int_pca = pca.fit_transform(scaler.fit_transform(ref_intensities))
-
-pca_ref_int_df = pd.DataFrame({"PC1": ref_int_pca[:, 0], "PC2": ref_int_pca[:, 1], "microscope": microscopes})
-fig = plt.figure(figsize=(10, 10))
-sns.scatterplot(x="PC1", y="PC2", hue="microscope", data=pca_ref_int_df, alpha=0.5)
-plt.title("PCA of Image Intensity Histograms")
-plt.legend(title="Microscope")
-plt.savefig(OUTPUT_DIR / "pca_scope.png")
-plt.clf()
-
-pca_ref_int_df = pd.DataFrame({"PC1": ref_int_pca[:, 0], "PC2": ref_int_pca[:, 1], "well_nums": wells})
-sns.scatterplot(x="PC1", y="PC2", hue="well_nums", data=pca_ref_int_df, alpha=0.5, legend=False)
-plt.title("PCA of Image Intensity Histograms")
-plt.savefig(OUTPUT_DIR / "pca_well.png")
-plt.clf()
-
-well_averages = []
-def well_to_scope(w):
-    if not w.split('--')[0] in scope_idx:
-        return "hpa"
-    return w.split('--')[0]
-scope_idx = {m: i for i, m in enumerate(set(microscopes))}
-well_idx = {w: i for i, w in enumerate(set(wells))}
-well_nums = np.array([well_idx[w] for w in wells])
-for w in set(wells):
-    well_averages.append(np.mean(ref_intensities[well_nums == well_idx[w]], axis=0))
-well_averages = pca.fit_transform(scaler.fit_transform(np.array(well_averages)))
-wells_types = list(set(wells))
-well_scopes = []
-well_scopes = np.array([well_to_scope(w) for w in wells_types])
-well_scope_nums = np.array([scope_idx[well_to_scope(w)] for w in wells_types])
-pca_well_avg_df = pd.DataFrame({"PC1": well_averages[:, 0], "PC2": well_averages[:, 1], "scope": well_scopes})
-sns.scatterplot(x="PC1", y="PC2", hue="scope", data=pca_well_avg_df, alpha=0.5)
-plt.title("Well-level Average of Non-zero Pixel Intensities")
-plt.legend(title="Microscope")
-plt.savefig(OUTPUT_DIR / "pca_well_averages.png")
-plt.clf()
+def well_percentile_averages(wells, microscopes, ref_intensities):
+    well_averages = []
+    def well_to_scope(w):
+        if not w.split('--')[0] in scope_idx:
+            return "hpa"
+        return w.split('--')[0]
+    scope_idx = {m: i for i, m in enumerate(set(microscopes))}
+    well_idx = {w: i for i, w in enumerate(set(wells))}
+    well_nums = np.array([well_idx[w] for w in wells])
+    for w in set(wells):
+        well_averages.append(np.mean(ref_intensities[well_nums == well_idx[w]], axis=0))
+    wells_types = list(set(wells))
+    well_scopes = np.array([well_to_scope(w) for w in wells_types])
+    return well_averages, well_scopes
